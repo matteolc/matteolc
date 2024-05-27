3a146fc95ad45acedfc273c4c81885710ce541d7
Matteo La Cognata <matteo.lacognata@gmail.com>
2024-05-27 10:28:27 +0200

    chore(): prepare actions

--- app/data/heading.ts
+++ app/data/heading.ts
@@ -2,7 +2,7 @@ const heading = {
   title: 'Curriculum vitae',
   name: 'Matteo La Cognata',
   role: 'Senior Software Engineer',
-  imageUrl: '/profile.jpeg',
+  imageUrl: '/matteolc/profile.jpeg',
   description:
     'I am a software engineer with over 10 years of experience in the field. I have worked in various companies and projects, from small startups to large corporations. I am pragmatic, never dogmatic and always open to debate. Although writing code is my main activity, I excel in design and I am very receptive in translating user pain-points into smooth user journeys. I am a team player and I enjoy mentoring junior members of the team. I am a strong advocate for mental health and I am always looking for ways to make the tech industry a more supportive place.',
 }
